baad7070fb6c63a8b73d7bf93e7d1927967ff77f
Ricardo Nardochone <ricardog.n@hotmail.com>
2022-06-14 14:17:30 -0300

    Made a few more sections

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 //NAVBAR
 const navLinks = document.querySelectorAll(".nav-link");
 
-function NavBarHandler (){
-    navLinks.forEach(link => {
+function NavBarHandler() {
+    navLinks.forEach((link) => {
         link.classList.remove("selected");
     });
     this.classList.add("selected");
 }
 
-navLinks.forEach(link => {
-    link.addEventListener("click", NavBarHandler)
+navLinks.forEach((link) => {
+    link.addEventListener("click", NavBarHandler);
+});
+
+
+//SWIPER
+var swiper = new Swiper(".mySwiper", {
+    effect: "cards",
+    grabCursor: true,
 });
